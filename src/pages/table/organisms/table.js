c3b2a7d524b615aae7b19b98812faf718c156211
hauseyo <rosadeyventi@gmail.com>
2020-09-02 14:17:16 +0300

    feat: create a new organism that renders table

---
+++ src/pages/table/organisms/table.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react"
+import { Table } from "antd"
+import { MyTag } from "../atoms/tag"
+
+const MyTable = () => {
+  const [data, setData] = useState()
+  const fetchData = () => {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://rs-react-schedule.firebaseapp.com/api/team/sh36team/events"
+    )
+      .then((response) => response.json())
+      .then((source) => setData(source.data))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const columns = [
+    {
+      title: "Начало",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Конец",
+      dataIndex: "deadline",
+      key: "deadline",
+    },
+    {
+      title: "Название",
+      dataIndex: "name",
+      key: "name",
+      render: (name) => <a href="/#">{name}</a>,
+    },
+    {
+      title: "Тип",
+      dataIndex: "type",
+      key: "type",
+      render: (type) => <MyTag type={type} />,
+    },
+    {
+      title: "Подробнее",
+      dataIndex: "datails",
+      key: "datails",
+    },
+  ]
+
+  return <Table columns={columns} dataSource={data} />
+}
+
+export { MyTable }
