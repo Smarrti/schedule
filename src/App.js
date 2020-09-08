1cf3f1dcf5d9e50b5c3ba3e42ccb11bd25738d45
hauseyo <rosadeyventi@gmail.com>
2020-09-08 20:48:09 +0300

    feat: wrapp app in dataprovider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react"
-
 import { MyTable } from "./pages/"
+import { DataProvider } from "./lib/fetching"
 
 const App = () => {
   return (
-    <>
+    <DataProvider>
       <MyTable />
-    </>
+    </DataProvider>
   )
 }
 
