44946e286ffb70622f07bc8373bdd062be083e01
hauseyo <rosadeyventi@gmail.com>
2020-09-09 00:08:09 +0300

    feat: create alias for pages

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,7 @@ module.exports = {
     alias: {
       "@ui": path.resolve(__dirname, "./src/ui"),
       "@lib": path.resolve(__dirname, "./src/lib"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
       "@features": path.resolve(__dirname, "./src/features"),
     },
   },
