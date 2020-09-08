976ea31ee0a2df7fd111151c9d941171a929491a
hauseyo <rosadeyventi@gmail.com>
2020-09-09 00:08:26 +0300

    feat: create tab, update imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from "react"
-import { MyTable } from "./pages/"
-import { DataProvider } from "./lib/fetching"
+import { Tabs } from "antd"
+import {
+  TableOutlined,
+  CalendarOutlined,
+  OrderedListOutlined,
+} from "@ant-design/icons"
+import { MyTable } from "@pages"
+import { DataProvider } from "@lib/fetching"
+
+const { TabPane } = Tabs
 
 const App = () => {
   return (
     <DataProvider>
-      <MyTable />
+      <Tabs defaultActiveKey="1">
+        <TabPane
+          tab={
+            <span>
+              <TableOutlined />
+              Таблица
+            </span>
+          }
+          key="1"
+        >
+          <MyTable />
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <CalendarOutlined />
+              Календарь
+            </span>
+          }
+          key="2"
+        >
+          Here must be a calendar!
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <OrderedListOutlined />
+              Список
+            </span>
+          }
+          key="3"
+        >
+          Here must be a list!
+        </TabPane>
+      </Tabs>
     </DataProvider>
   )
 }
