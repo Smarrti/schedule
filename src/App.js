2cb55f0367cd068955511da035b079f82938b4a5
hauseyo <rosadeyventi@gmail.com>
2020-09-09 20:47:28 +0300

    feat: wrapp app in size context

--- src/App.js
+++ src/App.js
@@ -7,47 +7,50 @@ import {
 } from "@ant-design/icons"
 import { MyTable } from "@pages"
 import { DataProvider } from "@lib/fetching"
+import { SizeContext } from "@lib/sizing"
 
 const { TabPane } = Tabs
 
 const App = () => {
   return (
     <DataProvider>
-      <Tabs defaultActiveKey="1">
-        <TabPane
-          tab={
-            <span>
-              <TableOutlined />
-              Таблица
-            </span>
-          }
-          key="1"
-        >
-          <MyTable />
-        </TabPane>
-        <TabPane
-          tab={
-            <span>
-              <CalendarOutlined />
-              Календарь
-            </span>
-          }
-          key="2"
-        >
-          Here must be a calendar!
-        </TabPane>
-        <TabPane
-          tab={
-            <span>
-              <OrderedListOutlined />
-              Список
-            </span>
-          }
-          key="3"
-        >
-          Here must be a list!
-        </TabPane>
-      </Tabs>
+      <SizeContext>
+        <Tabs defaultActiveKey="1">
+          <TabPane
+            tab={
+              <span>
+                <TableOutlined />
+                Таблица
+              </span>
+            }
+            key="1"
+          >
+            <MyTable />
+          </TabPane>
+          <TabPane
+            tab={
+              <span>
+                <CalendarOutlined />
+                Календарь
+              </span>
+            }
+            key="2"
+          >
+            Here must be a calendar!
+          </TabPane>
+          <TabPane
+            tab={
+              <span>
+                <OrderedListOutlined />
+                Список
+              </span>
+            }
+            key="3"
+          >
+            Here must be a list!
+          </TabPane>
+        </Tabs>
+      </SizeContext>
     </DataProvider>
   )
 }
