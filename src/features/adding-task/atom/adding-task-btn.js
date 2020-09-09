f4875d46498ebe40cee54a4eb8b2dd281a365d5b
Sergey Bashkin <bashkin2013@mail.ru>
2020-09-09 09:16:30 +0400

    feat: connected button from ant

--- src/features/adding-task/atom/adding-task-btn.js
+++ src/features/adding-task/atom/adding-task-btn.js
@@ -3,5 +3,11 @@ import { MyButton } from "@ui";
 import { handleAddingTask } from "../model/adding-task";
 
 export const AddingTaskBtn = () => {
-  
+  return (
+    <MyButton 
+      text="Добавить задачу"
+      type="primary"
+      onClick={handleAddingTask}
+    />
+  )
 }
