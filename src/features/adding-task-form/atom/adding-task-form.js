b3b074149db9f1093611b56af2617b062d5b9559
Sergey Bashkin <bashkin2013@mail.ru>
2020-09-11 08:49:02 +0400

    feat: added a few components on form

--- src/features/adding-task-form/atom/adding-task-form.js
+++ src/features/adding-task-form/atom/adding-task-form.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { Modal } from 'antd';
+import { Modal, Form, Input, DatePicker } from 'antd';
 // import { handleAddingTaskForm } from '../model/adding-task-form';
 
 export const AddingTaskForm = ({ 
   isModalForAddingTaskVisible, toggleModalForAddingTaskVisible
 }) => {
-  
+  const rulesOfInput = [{
+    required: true,
+    message: 'Пожалуйста заполните данное поле!'
+  }]
   return (
     <Modal
       title="Добавить задачу"
       visible={isModalForAddingTaskVisible}
     >
-
+      <Form
+        name="addingTaskForm"
+        preserve={false}
+      >
+        <Form.Item
+          label="Название задачи"
+          name="nameOfTask"
+          rules={rulesOfInput}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Дата"
+          name="dateOfTask"
+          rules={rulesOfInput}
+        >
+          <DatePicker />
+        </Form.Item>
+      </Form>
     </Modal>
   )
 }
