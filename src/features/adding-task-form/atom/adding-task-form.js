766099316d9dc2bb04cdbd890b7efda44a9148b3
Sergey Bashkin <bashkin2013@mail.ru>
2020-09-14 09:06:48 +0400

    feat: added fields of task

--- src/features/adding-task-form/atom/adding-task-form.js
+++ src/features/adding-task-form/atom/adding-task-form.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Modal, Form, Input, DatePicker } from 'antd';
+import { 
+  Modal, 
+  Form, 
+  Input, 
+  DatePicker, 
+  TimePicker, 
+  InputNumber,
+  Select
+} from 'antd';
+const { Option } = Select;
 // import { handleAddingTaskForm } from '../model/adding-task-form';
 
 export const AddingTaskForm = ({ 
@@ -34,6 +43,56 @@ export const AddingTaskForm = ({
           rules={rulesOfInput}
         >
           <DatePicker />
+        </Form.Item>
+        <Form.Item
+          label="Время"
+          name="timeOfTask"
+          rules={rulesOfInput}
+        >
+          <TimePicker
+            format="HH:mm"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Продолжительность в часах"
+          name="durationOfTask"
+          rules={rulesOfInput}
+        >
+          <InputNumber 
+            min={1}
+            max={10}
+            defaultValue={1}
+          />
+        </Form.Item>
+        <Form.Item
+          label="Лектор"
+          name="lecturerOfTask"
+          rules={rulesOfInput}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Формат"
+          name="formatOfTask"
+          rules={rulesOfInput}
+        >
+          <Select>
+            <Option value="lection">Лекция</Option>
+            <Option value="practik">Практическое занятие</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Теги (через запятую)"
+          name="tagsOfTask"
+          rules={rulesOfInput}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Доп. материалы"
+          name="materialsOfTask"
+        >
+          <Input />
         </Form.Item>
       </Form>
     </Modal>
