e142e4acaaf16e3f6a47af4cd96594fe19db5c8a
hauseyo <rosadeyventi@gmail.com>
2020-09-16 16:44:47 +0300

    feat: create a function to get items from ls

--- src/lib/customization/style-reducer.js
+++ src/lib/customization/style-reducer.js
@@ -3,6 +3,7 @@ import {
   CHANGE_TABLE_BG_PRACTICE,
   CHANGE_TABLE_FONT_COLOR_LECTURE,
   CHANGE_TABLE_FONT_COLOR_PRACTICE,
+  GET_TABLE_STYLES_FROM_LS,
   RESET_TABLE_STYLES,
 } from "./types"
 
@@ -51,6 +52,25 @@ export const styleReducer = (state, action) => {
         table: {
           ...state.table,
           practice: { ...state.table.practice, fontColor: action.payload },
+        },
+      }
+    case GET_TABLE_STYLES_FROM_LS:
+      return {
+        ...state,
+        table: {
+          ...state.table,
+          lecture: {
+            ...state.table.lecture,
+            background: localStorage.getItem("table-bg-lecture") || "blueBG",
+            fontColor:
+              localStorage.getItem("table-font-lecture") || "blackFont",
+          },
+          practice: {
+            ...state.table.practice,
+            background: localStorage.getItem("table-bg-practice") || "orangeBG",
+            fontColor:
+              localStorage.getItem("table-font-practice") || "blackFont",
+          },
         },
       }
     case RESET_TABLE_STYLES:
