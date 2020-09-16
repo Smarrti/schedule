819a8bd2c84d468d143506ab634e9b5e4e9fdd99
hauseyo <rosadeyventi@gmail.com>
2020-09-16 21:16:24 +0300

    feat: import function to change calendar size

--- src/features/sizing/calendar/molecule/calendar-sizing.js
+++ src/features/sizing/calendar/molecule/calendar-sizing.js
@@ -1,13 +1,17 @@
 import React, { useContext } from "react"
 import { RadioGroup, RadioButton } from "@ui"
 import { Size } from "@lib/sizing"
+import { changeCalendarSize } from "@lib/sizing"
 
 const CalendarSizing = () => {
   const { calendarSize, dispatch } = useContext(Size)
 
   console.log(calendarSize)
   return (
-    <RadioGroup defaultValue={calendarSize} onChange={() => dispatch()}>
+    <RadioGroup
+      defaultValue={calendarSize}
+      onChange={(e) => dispatch(changeCalendarSize(e.target.value))}
+    >
       <RadioButton value={false} text="Маленький" />
       <RadioButton value={true} text="Большой" />
     </RadioGroup>
