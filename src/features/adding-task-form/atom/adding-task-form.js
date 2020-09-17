c84075b00cda69d71a94732b58da72c8d2b4c0bc
Sergey Bashkin <bashkin2013@mail.ru>
2020-09-17 21:55:13 +0400

    refactor: added new generation of modal

--- src/features/adding-task-form/atom/adding-task-form.js
+++ src/features/adding-task-form/atom/adding-task-form.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { 
-  Modal, 
+import {
   Form, 
   Input, 
   DatePicker, 
@@ -9,6 +8,7 @@ import {
   Select,
   Button
 } from 'antd';
+import { WindowModal } from '@features/modal';
 import { handleAddingTaskForm } from '../model/adding-task-form';
 const { Option } = Select;
 
@@ -19,104 +19,109 @@ export const AddingTaskForm = ({
     required: true,
     message: 'Пожалуйста заполните данное поле!'
   }];
-  return (
-    <Modal
-      title="Добавить задачу"
-      visible={isModalForAddingTaskVisible}
-      onCancel={() => toggleModalForAddingTaskVisible(false)}
-      footer={[
-        <Button
-          key="close"
-          onClick={() => toggleModalForAddingTaskVisible(false)}
-        >
-          Закрыть
-        </Button>
-      ]}
-    >
-      <Form
-        name="addingTaskForm"
-        preserve={false}
-        onFinish={(values) => {
-          handleAddingTaskForm(values);
-          toggleModalForAddingTaskVisible(false);
-        }}
+  const modalProperties = {
+    title: "Добавить задачу",
+    visible: isModalForAddingTaskVisible,
+    onCancel: () => toggleModalForAddingTaskVisible(false),
+    footer: [
+      <Button
+        key="close"
+        onClick={() => toggleModalForAddingTaskVisible(false)}
       >
-        <Form.Item
-          label="Название задачи"
-          name="name"
-          rules={rulesOfInput}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Дата"
-          name="date"
-          rules={rulesOfInput}
-        >
-          <DatePicker />
-        </Form.Item>
-        <Form.Item
-          label="Время"
-          name="time"
-          rules={rulesOfInput}
-        >
-          <TimePicker
-            format="HH:mm"
-          />
-        </Form.Item>
-        <Form.Item
-          label="Продолжительность в часах"
-          name="duration"
-          rules={rulesOfInput}
-        >
-          <InputNumber 
-            min={1}
-            max={10}
-          />
-        </Form.Item>
-        <Form.Item
-          label="Лектор"
-          name="author"
-          rules={rulesOfInput}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Формат"
-          name="format"
-          rules={rulesOfInput}
-        >
-          <Select>
-            <Option value="lection">Лекция</Option>
-            <Option value="practik">Практическое занятие</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="Теги (через запятую)"
-          name="tags"
-          rules={rulesOfInput}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Место"
-          name="place"
-          rules={rulesOfInput}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Доп. материалы"
-          name="materials"
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item>
+        Закрыть
+      </Button>
+    ]
+  };
+  const modalContent = (
+    <Form
+      name="addingTaskForm"
+      preserve={false}
+      onFinish={(values) => {
+        handleAddingTaskForm(values);
+        toggleModalForAddingTaskVisible(false);
+      }}
+    >
+      <Form.Item
+        label="Название задачи"
+        name="name"
+        rules={rulesOfInput}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Дата"
+        name="date"
+        rules={rulesOfInput}
+      >
+        <DatePicker />
+      </Form.Item>
+      <Form.Item
+        label="Время"
+        name="time"
+        rules={rulesOfInput}
+      >
+        <TimePicker
+          format="HH:mm"
+        />
+      </Form.Item>
+      <Form.Item
+        label="Продолжительность в часах"
+        name="duration"
+        rules={rulesOfInput}
+      >
+        <InputNumber 
+          min={1}
+          max={10}
+        />
+      </Form.Item>
+      <Form.Item
+        label="Лектор"
+        name="author"
+        rules={rulesOfInput}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Формат"
+        name="format"
+        rules={rulesOfInput}
+      >
+        <Select>
+          <Option value="lection">Лекция</Option>
+          <Option value="practik">Практическое занятие</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        label="Теги (через запятую)"
+        name="tags"
+        rules={rulesOfInput}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Место"
+        name="place"
+        rules={rulesOfInput}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Доп. материалы"
+        name="materials"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item>
         <Button type="primary" htmlType="submit">
           Создать задачу
         </Button>
       </Form.Item>
-      </Form>
-    </Modal>
+    </Form>
+  );
+  return (
+    <WindowModal
+      properties={modalProperties}
+      content={modalContent}
+    />
   )
 }
