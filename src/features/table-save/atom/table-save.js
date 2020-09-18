bac44143caa03b12ca553920547a6cde8c583f1b
Sergey Bashkin <bashkin2013@mail.ru>
2020-09-18 08:46:35 +0400

    feat: added main information for table

--- src/features/table-save/atom/table-save.js
+++ src/features/table-save/atom/table-save.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { CSVLink } from 'react-csv'
 
 export const TableSave = () => {
+  const headers = [
+    { label: "First Name", key: "firstname" },
+    { label: "Last Name", key: "lastname" },
+    { label: "Email", key: "email" }
+  ];
 
+  const data = [
+    { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+    { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+    { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" }
+  ];
+
+  return (
+    <CSVLink data={data} headers={headers} />
+  )
 };
