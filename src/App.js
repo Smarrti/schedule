267fc5ba96584292662c39271b03700f570d28a3
hauseyo <rosadeyventi@gmail.com>
2020-09-19 19:05:14 +0300

    refactor: import pages from one source

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import {
   CalendarOutlined,
   OrderedListOutlined,
 } from "@ant-design/icons";
-import { MyTable } from "@pages";
-import { MyCalendar } from "@pages";
+import { MyTable, MyCalendar, Header } from "@pages";
 import { DataProvider } from "@lib/fetching";
 import { SizeContext } from "@lib/sizing";
 import { StyleContext } from "@lib/customization";
 import { RoleContext } from "@lib/roles";
-import { Header } from "./pages";
 
 const { TabPane } = Tabs;
 
