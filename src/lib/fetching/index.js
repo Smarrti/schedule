8b01ce1462b609f550f04269b5018be52ab6c6be
Sergey Bashkin <bashkin2013@mail.ru>
2020-09-21 09:09:49 +0400

    refactor: changed visibling tasks on table

--- src/lib/fetching/index.js
+++ src/lib/fetching/index.js
@@ -1,18 +1,12 @@
 import React, { createContext, useState, useEffect } from "react"
-
+import { getTasks } from '@lib/getTasks';
 export const DataContext = createContext()
 
 const DataProvider = ({ children }) => {
   const [data, setData] = useState(null)
 
-  const fetchData = () => {
-    fetch("https://rs-react-schedule.firebaseapp.com/api/team/sh36team/events")
-      .then((response) => response.json())
-      .then((source) => source.data.map((task) => {
-        task.key = task.id;
-        return task;
-      }))
-      .then((source) => setData(source))
+  const fetchData = async () => {
+    setData(await getTasks())
   }
 
   useEffect(() => {
